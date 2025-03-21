04883e0193a1e23f5e6d74601ac8c384a6e61af9
mahek1574 <shanishvaramahek1574@gmail.com>
2025-03-21 12:43:13 +0530

    Add basic calculation HTML and JS files

---
+++ basic calculation .js
@@ -0,0 +1,68 @@
+// ferenhite to celcius
+
+let ferenhite = 100;
+
+let celcius = ((ferenhite - 32) * 5) / 9;
+
+console.log(ferenhite + " ferenhite to celcius  = " + celcius + " celcius");
+
+// celcius to ferenhite
+
+let cel = 50;
+let fer = cel * (9 / 5) + 32;
+console.log(cel  + " celcius to ferenhite = " + fer + " ferenhite");
+
+// merter to foot
+
+let meter = 56;
+let foot = meter * 3.28084;
+
+console.log( meter + " meter to foot = " + foot + " foot");
+
+// milimeter to meter
+
+let milimeter = 1;
+
+let meter1 = milimeter / 1000;
+
+console.log(milimeter + " milimeter to meter = " + meter1 + " meter");
+
+// mile to km
+
+let mile = 20;
+let kilometer = mile * 1.6;
+
+console.log(mile + " mile to kilometer = " + kilometer + " kilometer");
+
+// km to mile
+
+let km = 15;
+let miles = km * 0.62137119;
+console.log(km  + " kelometer  to mile  = " + miles + " miles");
+
+// mile to meter
+
+let mile1 = 30;
+let meter2 = mile1 * 1609.344;
+console.log(mile1 + " mile to meter  = " + meter2 + " meter ");
+
+// second to hours
+
+seconds = 14400;
+let hours1 = seconds / 3600;
+
+console.log(seconds+ " seconds to hours =  " + hours1 + " hours");
+
+//  hours to mimutes
+let hours = 2;
+let minutes = hours * 60;
+console.log(hours + " hours to mimutes  = " + minutes + " minutes");
+
+// seconds to minutes 
+
+let sec = 300;
+let min = sec /60 ;
+
+console .log( sec  + " seconds to min =  " + min  + " minutes" );
+
+
